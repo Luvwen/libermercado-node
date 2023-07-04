a187972434e68c1be985830b6a5efa3ea1a1a668
Santiago Baliño <santiagobalino@hotmail.com>
2023-07-04 15:45:53 -0300

    Change env variable

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const DB_HOST = process.env.HOST || 'localhost';
 const DB_USER = process.env.USER || 'root';
 const DB_PASSWORD = process.env.PASSWORD || '';
 const DB_NAME = process.env.NAME || 'libremercado';
-const DB_PORT = process.env.PORT || 3306;
+const DB_PORT = process.env.PORT || 5406;
 
 module.exports = { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT };
