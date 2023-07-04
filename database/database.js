716dea5e1cda6b1cf4308e17cc35f4aa9a36c2f1
Santiago Baliño <santiagobalino@hotmail.com>
2023-07-04 16:20:43 -0300

    Delete config file

--- database/database.js
+++ database/database.js
@@ -1,13 +1,5 @@
+require('dotenv').config();
 const mysql = require('mysql2');
-
-const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, PORT } = require('../config');
-
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'libremercado'
-// });
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
